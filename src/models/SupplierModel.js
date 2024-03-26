af0573abe03dbdc3cf3c75b33e12c0d5f88c3430
LofA <82990903+lofadev@users.noreply.github.com>
2024-03-26 23:56:42 +0700

    create api for supplier (#5)

---
+++ src/models/SupplierModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const SupplierSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    contactName: { type: String, required: true },
+    province: { type: String, required: true },
+    address: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+const Supplier = mongoose.model('Supplier', SupplierSchema);
+export default Supplier;
--- src/routes/ShipperRouter.js
+++ src/routes/ShipperRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import ShipperController from '../controllers/ShipperController.js';
 import { authMiddleWare } from '../middlewares/authMiddleware.js';
+import { validateIdParam } from '../middlewares/validateMiddleware.js';
 
 const router = express.Router();
 
 router.post('/create', authMiddleWare, ShipperController.createShipper);
 router.get('/get-all', authMiddleWare, ShipperController.getShippers);
-router.get('/:id', authMiddleWare, ShipperController.getShipper);
-router.put('/update/:id', authMiddleWare, ShipperController.updateShipper);
-router.delete('/delete/:id', authMiddleWare, ShipperController.deleteShipper);
+router.get('/:id', validateIdParam, authMiddleWare, ShipperController.getShipper);
+router.put('/update/:id', validateIdParam, authMiddleWare, ShipperController.updateShipper);
+router.delete('/delete/:id', validateIdParam, authMiddleWare, ShipperController.deleteShipper);
 
 export default router;
