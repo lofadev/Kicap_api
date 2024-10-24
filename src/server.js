e7643c0e72e81fb24487aaa69746af99c200f04d
LofA <contact.lofa@gmail.com>
2024-10-25 00:04:37 +0700

    remove function delay

--- src/server.js
+++ src/server.js
@@ -1,30 +1,19 @@
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/index.js';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const port = process.env.PORT;
 const mongodbUrl = process.env.MONGODB_URL;
 const app = express();
 
-const delay = (time) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
-
-const midlewareDelay = async (req, res, next) => {
-  await delay(500);
-  next();
-};
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(midlewareDelay);
 
 try {
   await mongoose.connect(mongodbUrl);
